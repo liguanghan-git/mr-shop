efe3e28bc8957f04c5e425739ad374c54790b2c7
liguanghan <l15500000031@163.com>
2020-08-27 15:33:14 +0800

    qqqqq

--- mr-shop/mingrui-shop-basics/mingrui-shop-basics-eureka-server/src/main/java/com/baidu/RunEurekaServerApplication.java
+++ mr-shop/mingrui-shop-basics/mingrui-shop-basics-eureka-server/src/main/java/com/baidu/RunEurekaServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @ClassName TestEurekaFeignController
- * @Description: TODOlll
+ * @Description: TODOllllllll
  * @Author liguanghan
  * @Date 2020/8/27
  * @Version V1.0
